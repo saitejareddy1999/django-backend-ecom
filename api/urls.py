d8f4c118efdb25ad6f14666705b868d0c3415d36
saitejareddy1999 <saitejareddy5132@gmail.com>
2026-06-01 07:11:56 +0530

    second commit

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('Register/',user_views.RegisterView.as_view(), name='user-list'),
     path('Profile/', user_views.profileView.as_view(), name='user-profile'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
  
 ]
