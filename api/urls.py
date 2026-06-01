30fd5f98d37ea4967c313a94e4731a683f9ea987
saitejareddy1999 <saitejareddy5132@gmail.com>
2026-06-01 07:20:05 +0530

    ..

--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('Register/',user_views.RegisterView.as_view(), name='user-list'),
     path('Profile/', user_views.profileView.as_view(), name='user-profile'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-
-    
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
  
 ]
